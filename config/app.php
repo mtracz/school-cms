a7e2abf76ddb12500e50c1d3e58b717ac387ac2a
rfilipiuk <r.filipiuk@praktyki.itmdevlabs.com>
2017-07-05 09:21:36 +0200

    Installed: Laravel Collective Semantic UI

--- config/app.php
+++ config/app.php
@@ -180,6 +180,9 @@ return [
         // Debugbar
         Barryvdh\Debugbar\ServiceProvider::class,
 
+        //Laravel Collective
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -229,6 +232,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
