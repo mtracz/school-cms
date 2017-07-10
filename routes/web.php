98ff0ab1f3e6bf253a744993c3448ed8dbdf4300
marcin.tracz <marcin.tracz@itmdevlabs.com>
2017-07-10 13:42:32 +0200

    model auth

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', ["as" => "index.get", "uses" => "ViewController@index"]);
+
+Route::get("/login", ["as" => "login.get", "uses" => "ViewController@getLoginView"]);
