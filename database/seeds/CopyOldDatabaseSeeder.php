4d331ef2b77f8209bb15ed471e1c7c2f07eb0be5
rfilipiuk <r.filipiuk@praktyki.itmdevlabs.com>
2017-08-29 11:16:40 +0200

    #Add CopyOld Seeders

---
+++ database/seeds/CopyOldDatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CopyOldDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(CopyOldArticlesSeeder::class);
+        $this->call(CopyOldNewsSeeder::class);
+        $this->call(CopyOldSettingsSeeder::class);
+    }
+}
---
+++ database/seeds/CopyOldNewsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CopyOldNewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
