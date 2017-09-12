40efb7360f16ac5d02eba3a79173c2c645966fb7
marcin.tracz <marcin.tracz@itmdevlabs.com>
2017-09-12 13:19:45 +0200

    segregate routes

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,99 @@ Route::group(["middleware" => ["guest"]], function() {
 // LOGGED USERS
 Route::group(["middleware" => ["auth"]], function() {
 
-	
-
-});
-
-// LOGOUT ADMIN
+	// LOGOUT ADMIN//
 	Route::post("/logout", ["as" => "logout.post", "uses" => "Auth\LoginController@logout"]);
 
-Route::get("/global", ["as" => "global.get", "uses" => "GlobalController@serve"]);
+	// SITE SETTINGS//
+	Route::get('/settings/show', ["as" => "settings.show.get", "uses" => "SettingsController@getSettings"]);
+	Route::post('/settings/set', ["as" => "settings.set.post", "uses" => "SettingsController@setSettings"]);
+	Route::get('/settings', ["as" => "settings.get", "uses" => "ViewController@getSettings"]);
+	Route::post('/password/change', ["as" => "password.change.post", "uses" => "SettingsController@changePassword"]);
 
 
-//ALL USERS
+	//NEWS//
+	// news add view
+	Route::get('/news/add', ["as" => "news.add.get", "uses" => "ViewController@getNewsFormAdd"]);
+	// news add
+	Route::post('/news/add', ["as" => "news.add.post", "uses" => "NewsController@addNews"]);
+
+	//news edit view
+	Route::get('/news/edit/{id}', ["as" => "news.edit.get", "uses" => "ViewController@getNewsFormEdit"]);
+	//news edit 
+	Route::post('/news/edit/{id}', ["as" => "news.edit.post", "uses" => "NewsController@editNews"]);
+
+	// news delete
+	// //change to post
+	Route::post('/news/delete/{id}', ["as" => "news.delete.get", "uses" => "NewsController@deleteNews"]);
+
+	// news manage
+	Route::get("/news/manage", ["as" => "news.manage.get", "uses" => "ViewController@getNewsManagePage"]);
+
+
+	//STATIC PAGES//
+	//static page add view
+	Route::get('/page/add', ["as" => "page.add.get", "uses" => "ViewController@getPageFormAdd"]);
+	// page add
+	Route::post('/page/add', ["as" => "page.add.post", "uses" => "PageController@addPage"]);
+
+	//page edit view
+	Route::get('/page/edit/{id}', ["as" => "page.edit.get", "uses" => "ViewController@getPageFormEdit"]);
+	//page edit 
+	Route::post('/page/edit/{id}', ["as" => "page.edit.post", "uses" => "PageController@editPage"]);
+
+	// page delete
+	Route::post('/page/delete/{id}', ["as" => "page.delete.post", "uses" => "PageController@deletePage"]);
+
+	//page manage
+	Route::get('/page/manage', ["as" => "page.manage.get", "uses" => "ViewController@getPagesManagePage"]);
+
+
+	// FILES//
+	// add
+	Route::post("file/add", ["as" => "file.add.post", "uses" => "FileController@addFile"]);
+	// get files list
+	Route::get("file/list", ["as" => "file.list.get", "uses" => "FileController@getAllFilesFromServer"]);
+	// manage
+	Route::get("file/manage", ["as" => "file.manage.get", "uses" => "ViewController@getFilesManagePage"]);
+	// delete
+	Route::post("file/delete/{name}", ["as" => "file.delete.post", "uses" => "FileController@deleteFile"]);
+
+
+	// content tools
+	Route::post('/content_tools/upload_image', ["as" => "content_tools.image.upload.post", "uses" => "ContentToolsController@uploadImage"]);
+
+	Route::post('/content_tools/rotate_image', ["as" => "content_tools.image.rotate.post", "uses" => "ContentToolsController@rotateImage"]);
+
+	Route::post('/content_tools/save_image', ["as" => "content_tools.image.save.post", "uses" => "ContentToolsController@saveImage"]);
+
+	// ELEMENTS
+	// manage
+	Route::get("element/manage", ["as" => "element.manage.get", "uses" => "ViewController@getElementsManagePage"]);
+
+	// elements edit
+	Route::post("element/manage/update", ["as" => "element.update.post", "uses" => "ElementsController@updateElements"]);
+
+
+	// SITE ELEMENTS
+	// menu add view
+	Route::get("/elements/menu/add", ["as" => "menu.add.get", "uses" => "ViewController@getMenuAddView"]);
+	// menu add 
+	Route::post("/elements/menu/add", ["as" => "menu.add.post", "uses" => "ElementsController@addMenu"]);
+	// menu edit view
+	Route::get("/elements/menu/edit/{id}", ["as" => "menu.edit.get", "uses" => "ViewController@getMenuEditView"]);
+	// menu edit
+	Route::post("/elements/menu/edit/{id}", ["as" => "menu.edit.post", "uses" => "ElementsController@editMenu"]);
+	// menu delete 
+	Route::post("/elements/menu/delete/{id}", ["as" => "menu.delete.post", "uses" => "ElementsController@deleteMenu"]);
+
+
+
+});
+// end logged users group
+
+
+
+// ALL USERS //
 
 // MAIN PAGE
 Route::get('/', ["as" => "index.get", "uses" => "ViewController@index"]);
@@ -38,87 +120,12 @@ Route::get('/', ["as" => "index.get", "uses" => "ViewController@index"]);
 // MAINTENANCE PAGE
 Route::get("/maintenance", ["as" => "maintenance", "uses" => "ViewController@getMaintenancePage"]);
 
-// SITE SETTINGS
-
-Route::get('/settings/show', ["as" => "settings.show.get", "uses" => "SettingsController@getSettings"]);
-Route::post('/settings/set', ["as" => "settings.set.post", "uses" => "SettingsController@setSettings"]);
-Route::get('/settings', ["as" => "settings.get", "uses" => "ViewController@getSettings"]);
-
-Route::post('/password/change', ["as" => "password.change.post", "uses" => "SettingsController@changePassword"]);
-//NEWS//
-// news add view
-Route::get('/news/add', ["as" => "news.add.get", "uses" => "ViewController@getNewsFormAdd"]);
-// news add
-Route::post('/news/add', ["as" => "news.add.post", "uses" => "NewsController@addNews"]);
-
-//news edit view
-Route::get('/news/edit/{id}', ["as" => "news.edit.get", "uses" => "ViewController@getNewsFormEdit"]);
-//news edit 
-Route::post('/news/edit/{id}', ["as" => "news.edit.post", "uses" => "NewsController@editNews"]);
-
-// news delete
-// //change to post
-Route::post('/news/delete/{id}', ["as" => "news.delete.get", "uses" => "NewsController@deleteNews"]);
-
-// news manage
-Route::get("/news/manage", ["as" => "news.manage.get", "uses" => "ViewController@getNewsManagePage"]);
-
-//STATIC PAGES//
 //show page
 Route::get('/pages/{slug}', ["as" => "pages.show.get", "uses" => "ViewController@getPageView"]);
 
-//static page add view
-Route::get('/page/add', ["as" => "page.add.get", "uses" => "ViewController@getPageFormAdd"]);
-// page add
-Route::post('/page/add', ["as" => "page.add.post", "uses" => "PageController@addPage"]);
-
-//page edit view
-Route::get('/page/edit/{id}', ["as" => "page.edit.get", "uses" => "ViewController@getPageFormEdit"]);
-//page edit 
-Route::post('/page/edit/{id}', ["as" => "page.edit.post", "uses" => "PageController@editPage"]);
-
-// page delete
-// //change to post
-Route::post('/page/delete/{id}', ["as" => "page.delete.post", "uses" => "PageController@deletePage"]);
-
-//page manage
-Route::get('/page/manage', ["as" => "page.manage.get", "uses" => "ViewController@getPagesManagePage"]);
-
-// FILES
-// add
-Route::post("file/add", ["as" => "file.add.post", "uses" => "FileController@addFile"]);
-// get files list
-Route::get("file/list", ["as" => "file.list.get", "uses" => "FileController@getAllFilesFromServer"]);
-// manage
-Route::get("file/manage", ["as" => "file.manage.get", "uses" => "ViewController@getFilesManagePage"]);
-// delete
-Route::post("file/delete/{name}", ["as" => "file.delete.post", "uses" => "FileController@deleteFile"]);
-
-// ELEMENTS
-// manage
-Route::get("element/manage", ["as" => "element.manage.get", "uses" => "ViewController@getElementsManagePage"]);
-
-// elements edit
-Route::post("element/manage/update", ["as" => "element.update.post", "uses" => "ElementsController@updateElements"]);
 
 
 
-// content tools
-Route::post('/content_tools/upload_image', ["as" => "content_tools.image.upload.post", "uses" => "ContentToolsController@uploadImage"]);
 
-Route::post('/content_tools/rotate_image', ["as" => "content_tools.image.rotate.post", "uses" => "ContentToolsController@rotateImage"]);
 
-Route::post('/content_tools/save_image', ["as" => "content_tools.image.save.post", "uses" => "ContentToolsController@saveImage"]);
 
-// SITE ELEMENTS
-// menu add view
-Route::get("/elements/menu/add", ["as" => "menu.add.get", "uses" => "ViewController@getMenuAddView"]);
-// menu add 
-Route::post("/elements/menu/add", ["as" => "menu.add.post", "uses" => "ElementsController@addMenu"]);
-// menu edit view
-Route::get("/elements/menu/edit/{id}", ["as" => "menu.edit.get", "uses" => "ViewController@getMenuEditView"]);
-// menu edit
-Route::post("/elements/menu/edit/{id}", ["as" => "menu.edit.post", "uses" => "ElementsController@editMenu"]);
-// menu delete 
-// change to post!
-Route::post("/elements/menu/delete/{id}", ["as" => "menu.delete.post", "uses" => "ElementsController@deleteMenu"]);
