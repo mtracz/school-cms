ac27dc9d9cb3117bbd645e75caf89e0d62c72a23
marcin.tracz (DOM) <marcin.tracz@itmdevlabs.com>
2018-03-20 17:42:19 +0100

    authors in footer

--- resources/views/footerStaticData.blade.php
+++ resources/views/footerStaticData.blade.php
@@ -1,7 +1,16 @@
 <div class="row">
 
 	<div class="copyright">
-		<i class="copyright icon"></i> Szkoła Podstawowa Nr 9 w Legnicy, Wszystkie prawa zastrzeżone
+		<i class="copyright icon"></i> 
+		Szkoła Podstawowa Nr 9 w Legnicy, Wszystkie prawa zastrzeżone
+		<br><br>
+		<span style="font-size: 15px; font-style: italic;">
+			Designed & Created by
+			<br>
+			Marcin Tracz, Robert Filipiuk
+			<br>
+			2018
+		</span>
 	</div>
 
 </div>
